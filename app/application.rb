f0ce510823e29ba37fe68c9e0ca235a01fbff317
Laura Bodell <bodelllaura@gmail.com>
2018-04-28 18:03:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,22 @@
 class Application
+  
+  if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find {|i| i.name == item_name}
+      if item == nil
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        item = @@items.find {|i| i.name == item_name}
+        resp.write item.price
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+  
  
  end
  
