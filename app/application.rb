eeac447d36b1d80422713d3626cecd59d3b74ddb
Laura Bodell <bodelllaura@gmail.com>
2018-04-28 18:05:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,9 @@
 class Application
   
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+  
   if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_name}
